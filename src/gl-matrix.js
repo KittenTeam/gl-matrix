0d5cd5dbe7794265632cd1a1a9f640803a184b28
chengruilin <chengruilin@codemao.cn>
2018-09-20 16:16:32 +0800

    feat(gl-matrix): remove no use modules

--- src/gl-matrix.js
+++ src/gl-matrix.js
@@ -1,21 +1,3 @@
-import * as glMatrix from "./gl-matrix/common.js";
-import * as mat2 from "./gl-matrix/mat2.js";
-import * as mat2d from "./gl-matrix/mat2d.js";
-import * as mat3 from "./gl-matrix/mat3.js";
-import * as mat4 from "./gl-matrix/mat4.js";
-import * as quat from "./gl-matrix/quat.js";
-import * as quat2 from "./gl-matrix/quat2.js";
 import * as vec2 from "./gl-matrix/vec2.js";
-import * as vec3 from "./gl-matrix/vec3.js";
-import * as vec4 from "./gl-matrix/vec4.js";
 
-export { glMatrix }
-export { mat2 }
-export { mat2d }
-export { mat3 }
-export { mat4 }
-export { quat }
-export { quat2 }
 export { vec2 }
-export { vec3 }
-export { vec4 }
